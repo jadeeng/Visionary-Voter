c4a85a0585cffec5826ace9e6b0da07c84885a99
Kirsten Go <kirstengo568@gmail.com>
2019-07-24 15:00:46 -0700

    Added handlers to all of the pages on main.py

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import os
 import jinja2
 from datetime import datetime
 from datetime import timedelta
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
 
 class CalendarHandler(webapp2.RequestHandler):
     def get(self):
@@ -24,3 +29,25 @@ class CalendarHandler(webapp2.RequestHandler):
         #HTML link open in new tab taget="_blank"
         calendar_HTML = "<HTML><BODY><A href='%s' target='_blank'>Test Event Link</A></BODY></HTML>"
         self.response.write(calendar_HTML % calendar_link)
+
+class BlogPostHandler(webapp2.RequestHandler):
+    def get(self):
+        # [START user_details]
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        # [END user_details]
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/calendar', CalendarHandler)
+    ('/blogpost', BlogPostHandler)
+], debug=True)
